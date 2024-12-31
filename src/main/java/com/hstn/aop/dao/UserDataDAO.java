b485f8f493e92c6970716170a542a0e84dcde232
Jura Danilov <jura.danilov.2024@gmail.com>
2024-12-31 18:15:04 +0300

    Initial commit

---
+++ src/main/java/com/hstn/aop/dao/UserDataDAO.java
@@ -0,0 +1,7 @@
+package com.hstn.aop.dao;
+
+public interface UserDataDAO {
+    void addUserData();
+    // Интерфейсы хранят в себе только названия методов
+    //  А их имплементация происходит в классах
+}
