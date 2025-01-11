c94c1b88a0eee5c533598a0792fba5a80702dc51
Jura Danilov <jura.danilov.2024@gmail.com>
2025-01-11 22:18:12 +0300

    Initial commit

--- src/main/java/com/hstn/aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/hstn/aop/aspect/MyLoggingAspect.java
@@ -2,13 +2,18 @@ package com.hstn.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyLoggingAspect {
 
-    @Before("execution(public void addUserData())")
+    @Pointcut("execution(public void add*())")
+    private void pointcutForMethods() {
+    }
+
+    @Before("pointcutForMethods()")
 // Эта аннотация показывает перед каким методом будет вызван этот метод,
 // в параметрах этой аннотации указывается точка входа и сам тот метод
 // @Before - это эдвайс, а то, что дальше point (точечный) cut (разрез)
