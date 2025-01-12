75c9fb07b650cb2b33edc0f768fb6475be283a2c
Jura Danilov <jura.danilov.2024@gmail.com>
2025-01-12 23:13:06 +0300

    Initial commit

--- src/main/java/com/hstn/aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/hstn/aop/aspect/MyLoggingAspect.java
@@ -13,13 +13,33 @@ public class MyLoggingAspect {
     private void pointcutForMethods() {
     }
 
-    @Before("pointcutForMethods()")
+    @Pointcut("execution(* com.hstn.aop.dao.*.get*(..))")
+    private void pointcutForGetter() {
+    }
+
+    @Pointcut("execution(* com.hstn.aop.dao.*.set*(..))")
+    private void pointcutForSetter() {
+    }
+
+    @Pointcut("pointcutForSetter() || pointcutForGetter()")
+    private void pointcutForSetterAndGetter() {
+    }
+
+    @Pointcut("pointcutForMethods() && !(pointcutForSetter() || pointcutForGetter())")
+    private void pointcutNotForSetterAndGetter() {
+    }
+
+//    @Before("pointcutForMethods()")
+//    @Before("pointcutForGetter()")
+//    @Before("pointcutForSetter()")
+//    @Before("pointcutForSetterAndGetter()")
+    @Before("pointcutNotForSetterAndGetter()")
     public void beforeAddUserData() {
         System.out.println("    1 Before");
     }
 
-    @Before("pointcutForMethods()")
-    public void beforeAdmin() {
-        System.out.println("    3 Before");
-    }
+//    @Before("pointcutForMethods()")
+//    public void beforeAdmin() {
+//        System.out.println("    3 Before");
+//    }
 }
